812b7a91853938976af0a1748b902e7b8f75a402
jxhnkndl <jkroyston@gmail.com>
2022-03-24 18:03:28 -0400

    configure basic apollo server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const path = require('path');
 
+const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// init apollo server
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`GraphQL Playground at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+// start apollo server
+startServer();
 
 // req body parsing middleware
 app.use(express.urlencoded({ extended: false }));
