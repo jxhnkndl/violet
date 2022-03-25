25aed5e00edabbae9a8783bf1cade889a593033a
jxhnkndl <jkroyston@gmail.com>
2022-03-25 16:48:40 -0400

    create Mood model

---
+++ server/models/Mood.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const convertToNumber = require('../utils/parseInt');
+
+const moodSchema = new Schema(
+  {
+    mood: {
+      type: Number,
+      required: true,
+      get: moodRating => convertToNumber(moodRating)
+    },
+    anxiety: {
+      type: Number,
+      required: true,
+      get: anxietyRating => convertToNumber(anxietyRating)
+    },
+    insomnia: {
+      type: Number,
+      required: true,
+      get: insomniaRating => convertToNumber(insomniaRating)
+    },
+    symptoms: {
+      type: String
+    },
+    accomplishments: {
+      type: String
+    },
+    notes: {
+      type: String
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const Mood = model('Mood', moodSchema);
+
+module.exports = Mood;
