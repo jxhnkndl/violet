b30ef058b6393a1b514c8366c77d1a05611edde8
jxhnkndl <jkroyston@gmail.com>
2022-03-27 17:18:37 -0400

    initialize User and Mood type definitions

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,28 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    birthday: String
+    avatar: String
+    moods: [Mood]
+    moodCount: Int
+  }
+
+  type Mood {
+    _id: ID
+    date: String
+    mood: Int
+    anxiety: Int
+    insomnia: Int
+    panicAttacks: Int
+    symptoms: [String]
+    accomplishments: String
+    notes: String
+  }
+`;
 
 module.exports = typeDefs;
