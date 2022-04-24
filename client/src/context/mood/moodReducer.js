fce3828b6d771046ce9a6a16486d720a7e66f66f
jxhnkndl <jkroyston@gmail.com>
2022-04-24 18:00:47 -0400

    add switch cases for getting moods from api and setting mood loading status

--- client/src/context/mood/moodReducer.js
+++ client/src/context/mood/moodReducer.js
@@ -1,8 +1,24 @@
+import { GET_MOODS, SET_LOADING } from "../types";
 
 
-export default (state, action) => {
+const moodReducer = (state, action) => {
   switch (action.type) {
+    case GET_MOODS:
+      return {
+        ...state,
+        moods: action.payload,
+        loading: false
+      }
+
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      }
+
     default:
       return state;
   }
 }
+
+export default moodReducer;
