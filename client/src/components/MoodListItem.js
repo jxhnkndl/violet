249b57f2f72e93467601b46e0aeab943cbba4aa3
jxhnkndl <jkroyston@gmail.com>
2022-05-08 19:45:45 -0400

    create and style basic mood list item component

--- client/src/components/MoodListItem.js
+++ client/src/components/MoodListItem.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import dayjs from 'dayjs';
+
+import Row from 'react-bootstrap/Row';
+
+import IconInfo from '../assets/icons/icon-info.svg';
+import IconStable from '../assets/icons/icon-equals.svg';
 
 const MoodListItem = ({ data }) => {
-  const { _id, mood } = data;
+  const { _id, date, mood } = data;
 
   return (
-    <div>
-      <p>{_id}</p>
+    <div className="mood-list-item">
+      <div className="mood-data">
+        <div className="mood-icon">
+          <img src={IconStable} className="mood-list-icon" alt="" />
+        </div>
+        <div className="mood-details">
+          <p className="mood-date">{dayjs(date).format('MMMM D, YYYY')}</p>
+          <p className="mood-rating">Test</p>
+        </div>
+      </div>
+      <div className="info-icon">
+        <img src={IconInfo} className="mood-list-info" alt="" />
+      </div>
     </div>
   );
 };
