5d46e650063b61a54e1d2ea3209f16e55d36413c
jxhnkndl <jkroyston@gmail.com>
2022-05-15 19:03:24 -0400

    refactor page layout to move primary row and column components to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,23 @@ import MoodState from './context/mood/MoodState';
 import Container from 'react-bootstrap/Container';
 import Dashboard from './pages/Dashboard';
 import SingleMood from './pages/SingleMood';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <MoodState>
       <Router>
-        <div className="app">
-          <Container>
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/:moodId" element={<SingleMood />} />
-            </Routes>
-          </Container>
-        </div>
+        <Container>
+          <Row>
+            <Col lg={10} xl={8} className="column">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/:moodId" element={<SingleMood />} />
+              </Routes>
+            </Col>
+          </Row>
+        </Container>
       </Router>
     </MoodState>
   );
