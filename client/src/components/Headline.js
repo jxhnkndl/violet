41cfbfe3cf0641078adeed5e42aa689d3c6f43dc
jxhnkndl <jkroyston@gmail.com>
2022-05-15 19:20:31 -0400

    refactor headline component

--- client/src/components/Headline.js
+++ client/src/components/Headline.js
@@ -2,9 +2,14 @@ import React from 'react';
 import dayjs from 'dayjs';
 
 const Headline = (props) => {
+  // format mood chart date for display
+  const getDate = () => {
+    return dayjs().format('dddd MMMM D, YYYY');
+  };
+
   return (
     <div id="headline">
-      <p>Today is {dayjs().format('dddd MMMM D, YYYY')}</p>
+      <p>Today is {getDate()}</p>
       <h2>{props.headline}</h2>
     </div>
   );
